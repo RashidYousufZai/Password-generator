82bf1edad5e90757082820e9cd9ad8d9b0e285d9
Rashid <rashidyousufzai11@gmail.com>
2022-10-02 09:21:52 -0700

    updated

---
+++ Assets/app.js
@@ -0,0 +1,17 @@
+function passwordGenerator(){
+    var char = "qwertyuiopasdfghjklzxcvbnm1234567890!@#$%&";
+    var passsowrdLenght = 12;
+    var password = "";
+    for(let i = 0; i<passsowrdLenght; i++){
+        var random = Math.floor(Math.random()*char.length);
+        var password = password + char.substring(random,random+1);
+        console.log(password);
+    }
+    document.getElementById('password').value = password;
+}
+
+function copyPassword() {
+    var copyText = password;
+    copyText.select();
+    document.execCommand("copy");  
+}
